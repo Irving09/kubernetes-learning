0d9af9718a34eb44ea5787bfb1234bcb7bae0cfb
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2020-09-12 23:30:54 -0700

    added a heavy computation in nodejs to show up in metrics-server

--- app/node/app.js
+++ app/node/app.js
@@ -10,6 +10,9 @@ console.log(('port', listenerPort));
 const handler = (request, response) => {
   const clientIp = request.connection.remoteAddress;
   console.log('Received request for', request.url, 'from', clientIp);
+
+  const primes = calculatePrimes(50, 100000);
+
   response.writeHead(200);
   response.write(JSON.stringify({
     hostname: os.hostname(),
@@ -22,3 +25,22 @@ const handler = (request, response) => {
 
 const server = http.createServer(handler);
 server.listen(listenerPort);
+
+function calculatePrimes(iterations, multiplier) {
+  var primes = [];
+  for (var i = 0; i < iterations; i++) {
+    var candidate = i * (multiplier * Math.random());
+    var isPrime = true;
+    for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+      if (candidate % c === 0) {
+          // not prime
+          isPrime = false;
+          break;
+       }
+    }
+    if (isPrime) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
