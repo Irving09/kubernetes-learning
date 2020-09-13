e72c1302b2e76a0f61e1d82f85eb452e35c7e98d
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2020-09-13 10:35:49 -0700

    fix load scripts

--- utils/nodejs-load.sh
+++ utils/nodejs-load.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 minikube service --url hello-nodejs > nodejs-log.txt 2>&1 &
 sleep 5
-SERVICE_URL=$(sed '7q;d' nodejs-log.txt)
+SERVICE_URL=$(minikube ip)
 
 echo $SERVICE_URL
 
 while sleep 1; 
 do 
-  curl $SERVICE_URL/hello
+  curl $SERVICE_URL:30081/hello
 done
--- utils/springboot-load.sh
+++ utils/springboot-load.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 minikube service --url hello-springboot > springboot-log.txt 2>&1 &
 sleep 5
-SERVICE_URL=$(sed '7q;d' springboot-log.txt)
+SERVICE_URL=$(minikube ip)
 
 echo $SERVICE_URL
 
 while sleep 1; 
 do 
-  curl $SERVICE_URL/hello
-  curl $SERVICE_URL/helloNode;
+  curl $SERVICE_URL:30080/hello
+  curl $SERVICE_URL:30080/helloNode;
 done
