e1c6cf7b73e483aee3bbcf221fb23e95b0099168
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2020-10-11 22:53:13 -0700

    added nodejs instrumentation for jaeger

--- app/node/app.js
+++ app/node/app.js
@@ -58,13 +58,23 @@ function helloHandler(request, response) {
     message: 'hello response'
   };
 
-  tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, result)
+  tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, result);
   span.finish();
   response.send(JSON.stringify(result, null, 2));
 }
 
 let nextRetryHappens = 1;
 function mockRetryHandler(request, response, next) {
+  const tracer = opentracing.globalTracer();
+  const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, request.headers);
+  const span = tracer.startSpan(request.path, { childOf: wireCtx });
+  // Use the log api to capture a log
+  span.log({ event: 'request_received' });
+
+  // Use the setTag api to capture standard span tags for http traces
+  span.setTag(opentracing.Tags.HTTP_METHOD, request.method);
+  span.setTag(opentracing.Tags.SPAN_KIND, opentracing.Tags.SPAN_KIND_RPC_SERVER);
+  span.setTag(opentracing.Tags.HTTP_URL, request.path);
   nextRetryHappens = (nextRetryHappens + 1) % 4;
 
   const clientIp = request.connection.remoteAddress;
@@ -72,23 +82,29 @@ function mockRetryHandler(request, response, next) {
 
   const primes = calculatePrimes(300, 100000000);
   if (nextRetryHappens === 0) {
+    const result = {
+      hostname: os.hostname(),
+      clientIp: clientIp,
+      appVersion: appVersion,
+      message: 'NodeJS - hi response 503 retry'
+    };
+    tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, result);
+    span.finish();
     response
       .status(503)
-      .send(JSON.stringify({
-        hostname: os.hostname(),
-        clientIp: clientIp,
-        appVersion: appVersion,
-        message: 'NodeJS - hi response 503 retry'
-      }, null, 2));
+      .send(JSON.stringify(result, null, 2));
   } else {
-    response.send(JSON.stringify({
+    const result = {
       hostname: os.hostname(),
       clientIp: clientIp,
       appVersion: appVersion,
       app: 'NodeJS',
       message: 'hi response',
       nextRetryHappens: nextRetryHappens
-    }, null, 2));
+    };
+    tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, result);
+    span.finish();
+    response.send(JSON.stringify(result, null, 2));
   }
 }
 
